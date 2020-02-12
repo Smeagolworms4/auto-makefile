eb251162d32c4f476c5c29fdf5c84e3c854b039d
SmeagolWorms4 <smeagolworms4@gmail.com>
2020-02-13 00:23:04 +0100

    fix

--- docker.mk
+++ docker.mk
@@ -1,7 +1,7 @@
 export MAKEFILE_LIB_DIR=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 
 ifndef MAKEFILE_URL
-	export MAKEFILE_URL=127.0.0.1
+	export MAKEFILE_URL=https://raw.githubusercontent.com/Smeagolworms4/auto-makefile/master
 endif
 
 # ROOT #
@@ -13,14 +13,17 @@ include $(MAKEFILE_LIB_DIR)/root.mk
 # Docker #
 ##########
 
+.PHONY: list
 ## List all containers
 list: $(RULE_DEP_LIST)
 	$(RULE_CMD_LIST)
 
+.PHONY: killall
 ## Kill all containers for all projects
 killall: $(RULE_DEP_KILLALL)
 	$(RULE_CMD_KILLALL)
 
+.PHONY: clean-none
 ## Remove unammed image
 clean-none: $(RULE_DEP_CLEANNONE)
 	$(RULE_CMD_CLEANNONE)
